880ea32ec55d727967fa6d7f64b056a973f5a3d2
Daniel Vinther Andersen <dva@inropa.com>
2022-08-09 11:21:44 +0200

    Added CleanCodeExercise and GPOpgave and DataTypeDesc

---
+++ DataTypeDesc/Datatype.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace DataTypeDesc
+{
+    public static class Datatype
+    {
+        public static unsafe void PrintVariableInformation(ref sbyte datatype)
+        {
+            AnsiConsole.Write(new FigletText("sbyte").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            sbyte testVariable;
+            sbyte* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(sbyte).ToString(), sbyte.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref byte datatype)
+        {
+            AnsiConsole.Write(new FigletText("byte").LeftAligned().Color(Color.Red));
+            
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            byte testVariable;
+            byte* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(byte).ToString(), byte.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref short datatype)
+        {
+            AnsiConsole.Write(new FigletText("short").LeftAligned().Color(Color.Red));
+            
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            short testVariable;
+            short* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(short).ToString(), short.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref ushort datatype)
+        {
+            AnsiConsole.Write(new FigletText("ushort").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            ushort testVariable;
+            ushort* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(ushort).ToString(), ushort.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref int datatype)
+        {
+            AnsiConsole.Write(new FigletText("int").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            int testVariable;
+            int* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(int).ToString(), int.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref uint datatype)
+        {
+            AnsiConsole.Write(new FigletText("uint").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            uint testVariable;
+            uint* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(uint).ToString(), uint.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref long datatype)
+        {
+            AnsiConsole.Write(new FigletText("long").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            long testVariable;
+            long* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(long).ToString(), long.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref ulong datatype)
+        {
+            AnsiConsole.Write(new FigletText("ulong").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            ulong testVariable;
+            ulong* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(ulong).ToString(), ulong.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref float datatype)
+        {
+            AnsiConsole.Write(new FigletText("float").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            float testVariable;
+            float* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(float).ToString(), float.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref double datatype)
+        {
+            AnsiConsole.Write(new FigletText("double").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            double testVariable;
+            double* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(double).ToString(), double.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref decimal datatype)
+        {
+            AnsiConsole.Write(new FigletText("decimal").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            decimal testVariable;
+            decimal* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(decimal).ToString(), decimal.MaxValue.ToString("N2").Replace(",00", ""), add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+        public static unsafe void PrintVariableInformation(ref bool datatype)
+        {
+            AnsiConsole.Write(new FigletText("bool").LeftAligned().Color(Color.Red));
+
+            var table = new Table();
+            table.AddColumn("Size In Bytes");
+            table.AddColumn("Max Value");
+            table.AddColumn("Hex");
+
+            bool testVariable;
+            bool* pointer = &testVariable;
+            IntPtr add = (IntPtr)pointer;
+
+            table.AddRow(sizeof(bool).ToString(), "1", add.ToString("X"));
+
+            AnsiConsole.Write(table);
+        }
+    }
+}
---
+++ DataTypeDesc/Program.cs
@@ -0,0 +1,155 @@
+﻿using Spectre.Console;
+
+namespace DataTypeDesc
+{
+    internal class Program
+    {
+        public sealed class Fruit : IBarChartItem
+        {
+            public string Label { get; set; }
+            public double Value { get; set; }
+            public Color? Color { get; set; }
+
+            public Fruit(string label, double value, Color? color = null)
+            {
+                Label = label;
+                Value = value;
+                Color = color;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var items = new List<Fruit>
+            {
+                new Fruit("sbyte", 8, Color.Yellow),
+                new Fruit("byte", 8, Color.LightSalmon3_1),
+                new Fruit("short", 16, Color.LightCyan3),
+                new Fruit("ushort", 16, Color.HotPink_1),
+                new Fruit("char", 16, Color.Orange3),
+                new Fruit("string", 32, Color.Aqua),
+                new Fruit("int", 32, Color.GreenYellow),
+                new Fruit("array", 32, Color.Gold1),
+                new Fruit("uint", 32, Color.DarkCyan),
+                new Fruit("long", 64, Color.Chartreuse3),
+                new Fruit("ulong", 64, Color.CadetBlue),
+                new Fruit("float", 64, Color.BlueViolet),
+                new Fruit("double", 64, Color.Blue),
+                new Fruit("decimal", 128, Color.Red),
+            };
+            while (true)
+            {
+                AnsiConsole.Write(new FigletText("Data Type Info").LeftAligned().Color(Color.Blue));
+                AnsiConsole.WriteLine();
+                AnsiConsole.Write(new Markup("Following datatypes are avalible: [green]sbyte[/] : [green]byte[/]: [green]short[/]: [green]ushort[/]: [green]int[/]: [green]uint[/]: [green]long[/]: [green]ulong[/]: [green]float[/]: [green]double[/]: [green]decimal[/]: [green]bool[/]"));
+                AnsiConsole.WriteLine();
+                AnsiConsole.Write(new Markup("[orange3]Write [green]diagram[/] for a visual diagram of datatypes[/]"));
+                AnsiConsole.WriteLine();
+                Console.Write("Enter a datatype: ");
+                string? userinput = Console.ReadLine();
+                switch (userinput.ToLower())
+                {
+                    case "sbyte":
+                        {
+                            sbyte variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "byte":
+                        {
+                            byte variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "short":
+                        {
+                            short variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "ushort":
+                        {
+                            ushort variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "int":
+                        {
+                            int variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "uint":
+                        {
+                            uint variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "long":
+                        {
+                            long variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "ulong":
+                        {
+                            ulong variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "float":
+                        {
+                            float variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "double":
+                        {
+                            double variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "decimal":
+                        {
+                            decimal variable = 5;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+
+                    case "bool":
+                    case "boolean":
+                        {
+                            bool variable = true;
+                            Datatype.PrintVariableInformation(ref variable);
+                            break;
+                        }
+                    case "diagram":
+                        {
+                            AnsiConsole.WriteLine();
+                            AnsiConsole.Write(new BarChart().Width(64).Label("[green bold underline]Datatypes[/]").CenterLabel().AddItems(items));
+                            AnsiConsole.WriteLine();
+                            break;
+                        }
+                    case "exit":
+                    case "quit":
+                        {
+                            return;
+                        }
+                    default:
+                        AnsiConsole.Write(new Markup("[red]Invalid datatype[/]"));
+                        AnsiConsole.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
