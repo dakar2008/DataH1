a23d96bac654d7341d013de461cdf1384cce0dc6
Daniel Vinther Andersen <dva@inropa.com>
2022-08-10 13:50:04 +0200

    Changed UnitTest for QuickSort to Random Data Test

--- XUnitTestQuickSort/UnitTest1.cs
+++ XUnitTestQuickSort/UnitTest1.cs
@@ -1,50 +1,57 @@
 using QuickSort;
+using RandomNameGeneratorLibrary;
 using Xunit;
+using Xunit.Abstractions;
+
 namespace XUnitTestQuickSort
 {
     public class UnitTest1
     {
-        [Fact(DisplayName = "Integer Test 1")]
+        private readonly ITestOutputHelper _output;
+        public UnitTest1(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        [Fact(DisplayName = "Integer Test")]
         public void Test01()
         {
-            int[] arr = { 4, 3, 5, 1, 2 };
-            int n = arr.Length;
-            Assert.Equal(new int[] { 1, 2, 3, 4, 5 }, QuickSortClass.QuickSort(arr, 0, n - 1));
+            int testsToRun = 25;
+            int currentTestNumber = 0;
+            while (currentTestNumber < testsToRun)
+            {
+                Random rnd = new Random();
+                int arrayLength = rnd.Next(1, 25);
+                int[] arr = Enumerable.Repeat(0, arrayLength).Select(i => rnd.Next(1, 5000)).ToArray();
+                _output.WriteLine("Unsorted Array:");
+                _output.WriteLine($"[{string.Join(" , ", arr)}]");
+                int n = arr.Length;
+                Assert.Equal(arr.OrderBy(x => x).ToArray(), QuickSortClass.QuickSort(arr, 0, n - 1));
+                _output.WriteLine("Sorted Array:");
+                _output.WriteLine($"[{string.Join(" , ", arr)}]");
+                _output.WriteLine("--------------------------");
+                currentTestNumber++;
+            }
         }
-        [Fact(DisplayName = "Integer Test 2")]
+        [Fact(DisplayName = "String Test")]
         public void Test02()
         {
-            int[] arr = { 845, 12, 44, 105, 18, 280, 58, 360, 82, 624 };
-            int n = arr.Length;
-            Assert.Equal(new int[] { 12, 18, 44, 58, 82, 105, 280, 360, 624, 845 }, QuickSortClass.QuickSort(arr, 0, n - 1));
-        }
-        [Fact(DisplayName = "Integer Test 3")]
-        public void Test03()
-        {
-            int[] arr = { 25, 158, 5, 654, 2, 2500, 32, 3645, 6648, 33, 42, 487, 336, 8, 998 };
-            int n = arr.Length;
-            Assert.Equal(new int[] { 2, 5, 8, 25, 32, 33, 42, 158, 336, 487, 654, 998, 2500, 3645, 6648 }, QuickSortClass.QuickSort(arr, 0, n - 1));
-        }
-        [Fact(DisplayName = "String Test 1")]
-        public void Test04()
-        {
-            string[] arr = { "A", "C", "G", "H", "B", "K", "Q", "M" };
-            int n = arr.Length;
-            Assert.Equal(new string[] { "A", "B", "C", "G", "H", "K", "M", "Q" }, QuickSortClass.QuickSort(arr, 0, n - 1));
-        }
-        [Fact(DisplayName = "String Test 2")]
-        public void Test05()
-        {
-            string[] arr = { "Jens Peter", "Hans Madsen", "Julie Bertelsen", "Axel Gregersen", "Konrad Pedersen", "Aske Bak" };
-            int n = arr.Length;
-            Assert.Equal(new string[] { "Aske Bak", "Axel Gregersen", "Hans Madsen", "Jens Peter", "Julie Bertelsen", "Konrad Pedersen" }, QuickSortClass.QuickSort(arr, 0, n - 1));
-        }
-        [Fact(DisplayName = "String Test 3")]
-        public void Test06()
-        {
-            string[] arr = { "Chrysler", "Honda", "Volkswagen", "Dodge", "Audi", "BMW", "Cadillac", "Chevrolet" };
-            int n = arr.Length;
-            Assert.Equal(new string[] { "Audi", "BMW", "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Honda", "Volkswagen" }, QuickSortClass.QuickSort(arr, 0, n - 1));
+            int testsToRun = 25;
+            int currentTestNumber = 0;
+            while(currentTestNumber < testsToRun)
+            {
+                var placegenerator = new PlaceNameGenerator();
+                Random rnd = new Random();
+                int arrayLength = rnd.Next(1, 25);
+                string[] arr = placegenerator.GenerateMultiplePlaceNames(arrayLength).ToArray();
+                _output.WriteLine("Unsorted Array:");
+                _output.WriteLine($"[{string.Join(" , ", arr)}]");
+                int n = arr.Length;
+                Assert.Equal(arr.OrderBy(x => x).ToArray(), QuickSortClass.QuickSort(arr, 0, n - 1));
+                _output.WriteLine("Sorted Array:");
+                _output.WriteLine($"[{string.Join(" , ", arr)}]");
+                _output.WriteLine("--------------------------");
+                currentTestNumber++;
+            }
         }
     }
 }
