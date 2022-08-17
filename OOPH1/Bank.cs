116f74fb61749fe4dc36bbab4aa26ce84fcbeafb
Daniel Vinther Andersen <dva@inropa.com>
2022-08-17 13:47:00 +0200

    Created ScheduleAnalyzer and OOPH1 applications

---
+++ OOPH1/Bank.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPH1
+{
+    internal enum ActionType
+    {
+        Deposit,
+        Withdraw
+    }
+    internal enum PersonGender
+    {
+        Male,
+        Female
+    }
+
+    internal class Bank
+    {
+        /// <summary>
+        /// Name of the bank
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// List of customers
+        /// </summary>
+        public List<Customer> Customers { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type">Type of account action</param>
+        /// <param name="amount">Amount to deposit / withdraw</param>
+        /// <param name="cprNumber">Social Security Number</param>
+        /// <param name="accountNumber">Accountnumber for the account to peform action</param>
+        public void PerformAction(ActionType type, decimal amount, int customerId, int accountNumber)
+        {
+            Customer? customer = this.Customers.FirstOrDefault(x => x.Id == customerId);
+            if(customer != null)
+            {
+                Account? account = customer.Person.Accounts.FirstOrDefault(x => x.AccountNumber == accountNumber);
+                if (account != null)
+                {
+                    Action action = new Action();
+                    action.Amount = amount;
+
+                    if (type == ActionType.Withdraw)
+                    {
+                        if (account.Balance > amount)
+                        {
+                            account.Balance -= amount;
+                            action.Type = ActionType.Withdraw;
+
+                            Console.WriteLine($"{amount} withdraw from {account.Name}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Insufficient Founds!");
+                        }
+                    }
+                    else
+                    {
+                        account.Balance += amount;
+                        action.Type = ActionType.Deposit;
+
+                        Console.WriteLine($"{amount} depost to {account.Name}");
+                    }
+                    account.Actions.Add(action);
+                }
+                else
+                {
+                    Console.WriteLine($"Could not find account with id: {accountNumber}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Could not find a customer with social security number: {customerId}");
+            }
+        }
+    }
+    internal class Customer
+    {
+        /// <summary>
+        /// Customer Id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Person behind the customer
+        /// </summary>
+        public Person Person { get; set; }
+    }
+    internal class Person : Human
+    {
+        /// <summary>
+        /// Social Security Number
+        /// </summary>
+        public int CPR { get; set; }
+        /// <summary>
+        /// Name of the person
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Adress of the person
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Zipcode where the person lives
+        /// </summary>
+        public string ZipCode { get; set; }
+        /// <summary>
+        /// City where person lives
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// Gender of the person
+        /// </summary>
+        public PersonGender Gender { get; set; }
+        /// <summary>
+        /// Persons accounts
+        /// </summary>
+        public List<Account> Accounts { get; set; }
+    }
+    /// <summary>
+    /// Bank Account
+    /// </summary>
+    internal class Account
+    {
+        /// <summary>
+        /// Account number
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// Account Name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Balance of the account
+        /// </summary>
+        public decimal Balance { get; set; }
+        /// <summary>
+        /// Annual rate on the account
+        /// </summary>
+        public double AnnualRate { get; set; }
+        /// <summary>
+        /// Monthly service charges
+        /// </summary>
+        public double MonthlyServiceCharges { get; set; }
+        /// <summary>
+        /// Actions performed on the account
+        /// </summary>
+        public List<Action> Actions { get; set; }
+    }
+    internal class Action
+    {
+        /// <summary>
+        /// Amount withdraw / deposit
+        /// </summary>
+        public decimal Amount { get; set; }
+        /// <summary>
+        /// Action type
+        /// </summary>
+        public ActionType Type { get; set; }
+    }
+}
---
+++ OOPH1/Human.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPH1
+{
+    internal enum SidePlacement
+    {
+        Left = 0,
+        Right = 1,
+    }
+
+    internal class Human
+    {
+        public Head Head { get; set; }
+        public void CanSpeak()
+        {
+            Console.WriteLine("SPEAK SPEAK SPEAK SPEAK");
+        }
+        public void CanThink()
+        {
+            Console.WriteLine("THINKING THINKING THINKING THINKING");
+        }
+        public void CanHear()
+        {
+            Console.WriteLine("HEARING HEARING HEARING HEARING");
+        }
+    }
+
+    internal class Head
+    {
+        public Brain Brain { get; set; }
+        public List<Eye> Eyes { get; set; }
+        public List<Ear> Ears { get; set; }
+        public Nose Nose { get; set; }
+        public Mouth Mouth { get; set; }
+
+    }
+    internal class Brain
+    {
+        /// <summary>
+        /// Intelligence Quotient
+        /// </summary>
+        public int IQ { get; set; }
+        /// <summary>
+        /// Brain Capacity
+        /// </summary>
+        public int Capacity { get; set; }
+        /// <summary>
+        /// Is the brain alive or not
+        /// </summary>
+        public bool Alive { get; set; }
+    }
+    internal class Eye
+    {
+        /// <summary>
+        /// Placement of the eye, left or right
+        /// </summary>
+        public SidePlacement Placement { get; set; }
+        /// <summary>
+        /// Color of the eye
+        /// </summary>
+        public Color Color { get; set; }
+    }
+    internal class Ear
+    {
+        /// <summary>
+        /// Placement of the ear, left or right
+        /// </summary>
+        public SidePlacement Placement { get; set; }
+        /// <summary>
+        /// Size of the ear
+        /// </summary>
+        public int Size { get; set; }
+        /// <summary>
+        /// Is the ear working or not
+        /// </summary>
+        public bool Working { get; set; }
+    }
+    internal class Nose
+    {
+        /// <summary>
+        /// Size of the nose
+        /// </summary>
+        public int Size { get; set; }
+        /// <summary>
+        /// Is the nose flat or not
+        /// </summary>
+        public bool Flat { get; set; }
+        /// <summary>
+        /// Can the nose smell or not
+        /// </summary>
+        public bool CanSmell { get; set; }
+    }
+    internal class Mouth
+    {
+        /// <summary>
+        /// Width of the mouth in mm
+        /// </summary>
+        public int Width { get; set; }
+    }
+}
