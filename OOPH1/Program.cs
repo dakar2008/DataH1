48db2fe3292d8fb20919dc91acaff1e896ffe4d3
Daniel Andersen <daniel@nesupport.dk>
2022-08-24 23:16:13 +0200

    Played With TECHNOCOOL UI

--- OOPH1/Program.cs
+++ OOPH1/Program.cs
@@ -1,19 +1,12 @@
-﻿namespace OOPH1
+﻿using TECHCOOL.UI;
+
+namespace OOPH1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Bank bank = new Bank();
-            bank.Name = "My Bank";
-            if(bank == null)
-            {
-                Console.WriteLine("Bank Can't Be Empty");
-            }
-            else
-            {
-                bank.PerformAction(ActionType.Deposit, 12000, 12331, 1232131);
-            }
+            Screen.Display(new MenuScreen());
         }
     }
 }
