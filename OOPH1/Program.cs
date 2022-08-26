03148399b7bdbd22fee4f6e0f897a79e0fc0c546
Daniel Vinther Andersen <dva@inropa.com>
2022-08-26 09:45:15 +0200

    Revert "Played With TECHNOCOOL UI"
    This reverts commit 48db2fe3292d8fb20919dc91acaff1e896ffe4d3.

--- OOPH1/Program.cs
+++ OOPH1/Program.cs
@@ -1,12 +1,19 @@
-﻿using TECHCOOL.UI;
-
-namespace OOPH1
+﻿namespace OOPH1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Screen.Display(new MenuScreen());
+            Bank bank = new Bank();
+            bank.Name = "My Bank";
+            if(bank == null)
+            {
+                Console.WriteLine("Bank Can't Be Empty");
+            }
+            else
+            {
+                bank.PerformAction(ActionType.Deposit, 12000, 12331, 1232131);
+            }
         }
     }
 }
